77302357f66e124884b5770e494e299d0d43b931
Vishal Sharma <vishalsharma@ip-192-168-1-101.ap-south-1.compute.internal>
2023-05-17 21:56:46 +0530

    17 may first commit

--- Tutorial.js
+++ Tutorial.js
@@ -1,1 +1,36 @@
-console.log("hey"); // debugging
+// console.log("First Javascript code line");
+
+// chrome developer tools
+
+// browser developer tools
+
+// variable
+
+// for storing the data
+// whole the world all the website , software everything is nothing without the
+// data
+
+// text , number  , array , true/false
+
+// java  => diff , diff data tyep for variable
+
+// it is dynamic type conversion langauge
+//it is not  static typed language => JAVA
+
+//In Javascript we have three keyword which is used for defining or
+// creating a  variable => var , let , const
+
+// var => form initial  (before 7 June 2015)
+// let and const came on ecmascript=>6 => es6
+
+// var
+
+// in javascript anything inside the single quoted , double quoted and  ``(back-tick)
+var firstName = "Vishal";
+var lastName = "Sharma";
+var middleName = `Joshi`;
+console.log(firstName, middleName, lastName);
+
+// typeof keyword will tell you what is the data type of variable
+
+console.log(typeof middleName, typeof lastName, typeof firstName);
