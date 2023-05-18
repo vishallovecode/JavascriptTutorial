913edb71734dd17c5c4e98cce6754e3cde2b34c8
Vishal Sharma <vishalsharma@ip-192-168-1-100.ap-south-1.compute.internal>
2023-05-18 22:32:57 +0530

    parseInt , parseFloat , implicit type conversion

--- Tutorial2.js
+++ Tutorial2.js
@@ -22,3 +22,75 @@ if (middleName) {
 } else {
   console.log("No value");
 }
+
+// Number
+
+var a = 30;
+var b = 0;
+var c = -40;
+var d = -0;
+// typeof b ==> number
+if (a & c) {
+  console.log("Hey a and c both are true===>  true&true=true");
+}
+if (b & c) {
+  console.log("Hey b is false and c is true=>>", "false&true=false");
+} else {
+  console.log("Hey b is false and c is true=>>", "false&true=false");
+}
+if (b) {
+  console.log("b is true");
+} else {
+  console.log("b is false");
+}
+if (d) {
+  console.log("d is true");
+} else {
+  console.log("d is false");
+}
+
+// const check  = true  / false
+// in the
+
+// if()
+// conditianlly writing something
+
+// var array = [];
+// var object = {};
+
+// Explicit Type conversion
+
+var num1 = "30"; //=> Numeric string value
+//if there us any value which is numeric string you can convert that value into number using parseInt
+
+var num2 = parseInt(num1); // deliberately i am changing the type of num1 variable
+
+console.log("Num2=> ", typeof num2, "Num1=>", typeof num1);
+
+// parseInt it will return the number if  value passed as parameter is numeric string value , apart from this
+// if we will pass anything this will return NAN => not a number
+// parseInt() => number or NaN
+var collegeName = "Newton";
+var collegeNameConvert = parseInt(collegeName);
+console.log("collegeNameConvert", collegeNameConvert);
+console.log(typeof NaN); // number
+
+console.log(parseInt("40")); //=> string numeric => 40
+console.log(parseInt("BASDYJV")); // => string =>NaN
+
+// parseFloat return number with decimal
+
+var float1 = "1234.5888888";
+var floatNumber = parseFloat(float1);
+console.log(floatNumber);
+
+var float12 = "1234.57777777777777777777";
+var floatNumber1 = parseInt(float1);
+console.log(floatNumber1);
+
+// converting into the boolean
+
+var ls = "Vishal";
+var ls2 = " ";
+console.log(Boolean(ls)); // true
+console.log(Boolean(ls2)); // false
