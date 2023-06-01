d9b87dd64c9feedcd32efd6f3a9f26802d5d39e3
Vishal Sharma <vishalsharma@Vishals-MacBook-Pro.local>
2023-06-01 22:32:28 +0530

    try catch

--- tryCatch.js
+++ tryCatch.js
@@ -1,5 +1,4 @@
-//
-
+// Error Type 1
 var newton = {};
 var userDetail = {
   name: "Vishal",
@@ -29,3 +28,42 @@ const cc = {
 // this will through the error
 // cc.name.name1.name.x // undefined
 // cc.name.name1.name.x.y; //  // this will through the error
+
+// Error Type 2
+
+// let var const
+// const ff;   =>  this will throw the error initilization is required for const variable
+const ff = "Flower";
+console.log(ff);
+// ff = "Vegetable"; // you cant reassign value to const variable error => Uncaught TypeError: Assignment to constant variable.
+
+// Error Type 3
+
+// Object.keys() , Object.values , length
+
+const object = {
+  a: 1,
+  b: 2,
+  c: 3,
+};
+console.log(object.length);
+
+const array = [1, 2, 3, 4];
+
+console.log(Object.keys(array));
+
+try {
+  array.trim();
+} catch (error) {
+  console.log(
+    "I can handle the error here if occureed  , I can show ui later when we study the dom or react or i can proper meessage"
+  );
+  console.log(error);
+}
+
+console.log("hey");
+
+// try {
+// } catch (error) {}
+
+// array is object only
