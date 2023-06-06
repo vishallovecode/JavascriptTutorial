5204a74e9b3579d3927e6567b20dd2655762968a
Vishal Sharma <vishalsharma@ip-192-168-1-102.ap-south-1.compute.internal>
2023-06-06 23:03:59 +0530

    let and const tempporal dead zone

--- exectioncontext.js
+++ exectioncontext.js
@@ -17,3 +17,13 @@ console.log(a);
 
 // hoisting is the process of allocating memory to the javascript variable due to  this process , you can access
 // variable before its declration or you can access variable top of it declaration
+
+// console.log(b);
+
+// let b = 30;
+// console.log(b);
+
+// console.log(c);
+
+let c = 30;
+console.log(c);
