2b9f06bc471ae178552f4cf5ea6c8b57ca226d4c
Vishal Sharma <vishalsharma@ip-192-168-1-101.ap-south-1.compute.internal>
2023-06-07 22:09:07 +0530

    function execution context

--- exectioncontext.js
+++ exectioncontext.js
@@ -1,31 +1,50 @@
-// Single Threaded
-// Synchrnous
-// AsynChrnous
-// Multi Threaded
-// Execution Context
-// Type Of execution context
+// // Single Threaded
+// // Synchrnous
+// // AsynChrnous
+// // Multi Threaded
+// // Execution Context
+// // Type Of execution context
 
-// mallinath
-// neha
-// dilon
+// // mallinath
+// // neha
+// // dilon
+
+// console.log(a);
+
+// var a = 20;
+
+// console.log(a);
+
+// // hoisting is the process of allocating memory to the javascript variable due to  this process , you can access
+// // variable before its declration or you can access variable top of it declaration
+
+// // console.log(b);
+
+// // let b = 30;
+// // console.log(b);
+
+// console.log(c); // temporal dead zone
+
+// let c = 30;
+// console.log(c);
+
+// // Temporal Dead Zone  => the period of time during which the let and const declarations cannot be accessed
 
 console.log(a);
+var a = 30;
 
-var a = 20;
+// console.log(c);
+const c = 30;
 
-console.log(a);
+function call(a, b, c) {
+  var result = a + b + c;
 
-// hoisting is the process of allocating memory to the javascript variable due to  this process , you can access
-// variable before its declration or you can access variable top of it declaration
+  console.log("helo0");
 
-// console.log(b);
+  return result;
+}
 
-// let b = 30;
-// console.log(b);
+// console.log(result);
+let result = call(10, 20, 30);
 
-console.log(c); // temporal dead zone
-
-let c = 30;
-console.log(c);
-
-// Temporal Dead Zone  => the period of time during which the let and const declarations cannot be accessed
+console.log(result);
