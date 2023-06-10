a2cb5d5d576bc8226a43097d7ae1fe771077282b
Vishal Sharma <vishalsharma@ip-192-168-1-100.ap-south-1.compute.internal>
2023-06-10 23:02:54 +0530

    var function scope

--- Scope.js
+++ Scope.js
@@ -19,18 +19,18 @@
 // var is functional scope
 // let and const is block scope
 
-if (true) {
-  var lastName = "SHARMA";
-  let firstName = "VISHAL";
-  const city = "Kanpur";
-  console.log(lastName); // SHARMA
-  console.log(firstName); // VISHAL
-  console.log(city); // Kanpur
-}
+// if (true) {
+//   var lastName = "SHARMA";
+//   let firstName = "VISHAL";
+//   const city = "Kanpur";
+//   console.log(lastName); // SHARMA
+//   console.log(firstName); // VISHAL
+//   console.log(city); // Kanpur
+// }
 
-console.log(lastName); // SHARMA
-console.log(firstName); // error
-console.log(city); // error
+// console.log(lastName); // SHARMA
+// console.log(firstName); // error
+// console.log(city); // error
 // // block scope
 // if(true) {
 
@@ -41,3 +41,26 @@ console.log(city); // error
 // }
 
 // // while switch
+
+var a = 40;
+function varScope() {
+  console.log(a); // 40
+  var a = 60;
+  console.log(c); // error , undefined
+  var c = 60;
+  console.log(s); // error  tdz
+  let s = 30;
+  console.log(ss); // error  tdz
+  const ss = 60;
+  console.log("a", a);
+  console.log("c", c);
+  console.log("s", s);
+  console.log("ss", ss);
+}
+
+varScope();
+console.log("outside=====");
+console.log("a", a);
+console.log("c", c); // error
+console.log("s", s); // error
+console.log("ss", ss); // error
