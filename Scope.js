540c29230bf1c95ec8641a0d90509aee915df146
Vishal Sharma <vishalsharma@ip-192-168-1-100.ap-south-1.compute.internal>
2023-06-10 22:40:25 +0530

    let and const block scope

--- Scope.js
+++ Scope.js
@@ -1,10 +1,43 @@
-var a = 40;
+// var a = 40;
 
-function call() {
-  a = 60;
-  let b = 500;
-  console.log(a);
+// function call() {
+//   a = 60;
+//   let b = 500;
+//   console.log(a);
+// }
+
+// call();
+// console.log(b);
+
+// scope => the area or boundary or environment where you can access the defined variable
+// lexical environment => its own scope + its parent scope
+
+// var  => redeclared, reassign  , it will be not in tdz , top declaration we can access
+// let => reassign  , it will be  in tdz , top declaration we cant access
+// cont => it will be  in tdz , top declaration we cant access
+
+// var is functional scope
+// let and const is block scope
+
+if (true) {
+  var lastName = "SHARMA";
+  let firstName = "VISHAL";
+  const city = "Kanpur";
+  console.log(lastName); // SHARMA
+  console.log(firstName); // VISHAL
+  console.log(city); // Kanpur
 }
 
-call();
-console.log(b);
+console.log(lastName); // SHARMA
+console.log(firstName); // error
+console.log(city); // error
+// // block scope
+// if(true) {
+
+// }
+// // block scope
+// for() {
+
+// }
+
+// // while switch
---
+++ Team A.sh
@@ -0,0 +1,45 @@
+Team A 
+malliath 
+sudhanshu-sekher
+nikhil
+santosh
+vipulgupta
+ramaprabha
+gagandeep
+dilip
+sumitpal
+
+
+
+
+Team b => 1 + 1
+sarvesh
+bharat anand
+ram lavnia
+sankar rao
+sairam
+surjeet singh
+mafeha praveen
+devi purnima
+
+
+Team c =>1
+neha
+adarsh
+vishnu
+harsh bhungaliya
+vijay jain
+hemant
+ravi anand
+abhik Sahana
+
+
+Team d => 1
+Sanskar Bharadwaj
+HANMAKONDA VEERACHARY
+Brajaditya Singh
+vipul Goyal
+Shadab Ahmad 
+Anurag Ranjan
+mohd Javid
+rahul
