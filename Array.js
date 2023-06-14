632483eafbc257282986ebefeee1d8b130e83db3
Vishal Sharma <vishalsharma@ip-192-168-1-102.ap-south-1.compute.internal>
2023-06-14 22:24:36 +0530

    join and split

--- Array.js
+++ Array.js
@@ -19,7 +19,7 @@ const a = [
   false,
   "Hey i am array",
   function () {
-    console.log("hey");
+    return 8;
   },
 ];
 
@@ -27,6 +27,62 @@ function callme() {
   console.log("hey");
 }
 
-console.log(a);
-console.log(callme());
-console.log(a[7]());
+// console.log(a);
+// console.log(callme());
+// console.log(a[7]());
+
+for (let value of a) {
+  console.log(value);
+}
+for (let index in a) {
+  console.log(index);
+  break;
+}
+
+//imperative approach
+
+for (let i = 0; i < a.length; i++) {
+  console.log(a[i]);
+  break;
+}
+
+// method   Js
+
+// for of
+// for in
+// loop method
+// forEach
+//======>// map
+// reduce
+// filter
+// some
+// any
+// every
+
+// reverse string
+
+// split()
+
+const s = "vishal"; // => [v , i , s , h , a , l]
+
+const setence = "Hey I am Vishal Sharma";
+
+const array = s.split("");
+console.log(array);
+console.log(s.split("s"));
+
+const array23 = setence.split(" ");
+console.log(typeof array23); // Object
+
+const commadata = "Hey , I am vishal Sharma, I am from kanpur uttar pradesh";
+const array4 = commadata.split(",");
+
+// Join function
+
+// 1-2-3-4-4-5
+const data = [1, 2, 3, 4, 4, 5];
+
+const ss = data.join("-");
+console.log(ss);
+
+console.log(typeof data.join("/")); // string
