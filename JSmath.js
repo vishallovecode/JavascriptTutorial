847cb4ad59040c851d05211df4b55edb2850f472
Vishal Sharma <vishalsharma@ip-192-168-1-100.ap-south-1.compute.internal>
2023-06-27 22:37:49 +0530

    js math

--- JSmath.js
+++ JSmath.js
@@ -46,3 +46,31 @@ console.log(Math.ceil(6.1)); // 7
 
 console.log(Math.round(5.6)); // 6
 console.log(Math.floor(5.9)); // 5
+
+// generate whole positive between min to max  here min  and max should be the part of range
+
+function generateRandomNumber(max, min) {
+  return Math.ceil(Math.random() * (max - min + 1) + min);
+}
+
+generateRandomNumber(20, 100); //  20 ... 99.999999 => 100
+
+Math.max(0, -1, 1, 2, 2, 3, 4, 5, 6, 7, 9000); //  9000
+
+const array = [100, 200, 300, 400, 500, 12, 30, 40];
+
+// one function which takes a array as input and return maximum element from array using math.max
+
+function getMax(arr) {
+  return Math.max(...arr);
+}
+
+function getMan(arr) {
+  return Math.min(...arr);
+}
+
+const maxSum = getSum(array); // 500
+const maxMin = getMin(array); // 500
+
+console.log("maxSum", max);
+console.log("maxSum", maxMin);
