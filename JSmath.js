296d22f94392cfe705c46aaac6ced2443eb2b4d0
Vishal Sharma <vishalsharma@ip-192-168-1-100.ap-south-1.compute.internal>
2023-06-27 21:41:04 +0530

    max and min range randowm numbver

--- JSmath.js
+++ JSmath.js
@@ -1,11 +1,11 @@
 // keyword
-console.log(Math.random() * 10); // Object
+// console.log(Math.random() * 10); // Object
 
-for (let i = 0; i < 100; i++) {
-  console.log(Math.random() * 10);
-}
+// for (let i = 0; i < 100; i++) {
+//   console.log(Math.random() * 10);
+// }
 
-// Math.random() //
+// Math.random() // it will only give the value between 0 to 1 where 1 is exclusive
 
 const MyMath = {
   E: 2.718281828459045,
@@ -18,3 +18,15 @@ const MyMath = {
 // max
 // min
 // sqrt
+
+//
+
+// generate random betweeen given to number
+// max and min
+
+let max = 100;
+let min = 20;
+// min should be part of random number and max should not be the part of random number
+
+const randNo = Math.random() * (max - min) + min;
+console.log("Hey", randNo);
