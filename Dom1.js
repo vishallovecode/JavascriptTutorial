0e2c076e9f749dade58e060af59a33270d888b54
Vishal Sharma <vishalsharma@ip-192-168-1-100.ap-south-1.compute.internal>
2023-06-28 22:27:04 +0530

    script tag

--- Dom1.js
+++ Dom1.js
@@ -1,16 +1,10 @@
-console.log("DOM Tutorial");
+console.log("SCRIPT2");
 
 const rootElement = document.getElementById("root"); // null
 
 rootElement.innerText = "Hey Dom Tutorial";
 rootElement.style.fontSize = "34px";
-rootElement.style.color = "red";
+rootElement.style.color = "blue";
 rootElement.style.margin = "auto";
 rootElement.style.width = "100%";
 rootElement.style.textAlign = "center";
-
-console.log(rootElement, "hey");
-
-// setTimeout(() => {
-//   console.log("DOM Tutorial11");
-// }, 5000);
