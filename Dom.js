c5527283132edc99becfacf378ace1fa46d20660
Vishal Sharma <vishalsharma@ip-192-168-1-100.ap-south-1.compute.internal>
2023-06-28 22:02:01 +0530

    dom intro

---
+++ Dom.js
@@ -0,0 +1,12 @@
+console.log("DOM Tutorial");
+
+const rootElement = document.getElementById("root");
+
+rootElement.innerText = "Hey Dom Tutorial";
+rootElement.style.fontSize = "34px";
+rootElement.style.color = "red";
+rootElement.style.margin = "auto";
+rootElement.style.width = "100%";
+rootElement.style.textAlign = "center";
+
+console.log(rootElement, "hey");
