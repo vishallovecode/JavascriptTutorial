1c7d500877e92da2bc924d091334d0cec8299042
Vishal Sharma <vishalsharma@ip-192-168-1-100.ap-south-1.compute.internal>
2023-07-01 21:33:18 +0530

    dom explanation

--- Dom.js
+++ Dom.js
@@ -2,15 +2,11 @@ console.log("DOM Tutorial");
 
 const rootElement = document.getElementById("root"); // null
 
+const root = document.getElementsByClassName("firstDiv");
+
 rootElement.innerText = "Hey Dom Tutorial";
 rootElement.style.fontSize = "34px";
 rootElement.style.color = "red";
 rootElement.style.margin = "auto";
 rootElement.style.width = "100%";
 rootElement.style.textAlign = "center";
-
-console.log(rootElement, "hey");
-
-// setTimeout(() => {
-//   console.log("DOM Tutorial11");
-// }, 5000);
