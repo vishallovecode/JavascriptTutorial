4236133c0c6ba52f8bcf43d389364940c97fd05a
Vishal Sharma <vishalsharma@ip-192-168-1-100.ap-south-1.compute.internal>
2023-07-01 22:01:45 +0530

    createElement

--- Dom.js
+++ Dom.js
@@ -10,3 +10,16 @@ rootElement.style.color = "red";
 rootElement.style.margin = "auto";
 rootElement.style.width = "100%";
 rootElement.style.textAlign = "center";
+
+// We have created new div using createElement
+const root2 = document.createElement("div"); //Node
+root2.innerText = "Hey this is dynamically added div";
+root2.style.fontSize = "34px";
+root2.style.color = "red";
+root2.style.margin = "auto";
+root2.style.width = "100%";
+root2.style.textAlign = "center";
+
+console.log(root2, "root2");
+
+document.body.appendChild(root2);
