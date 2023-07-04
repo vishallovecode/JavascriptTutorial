c785c992986853ed1378d59c27ea78af6932067c
Vishal Sharma <vishalsharma@Vishals-MacBook-Pro.local>
2023-07-04 23:09:47 +0530

    addEventListener

---
+++ dom2.js
@@ -0,0 +1,19 @@
+// create one button on clicking that you need to change the color of another div
+// color inputwill ['red' ,'blue' , 'green' , 'yellow]
+
+const array = ["red", "blue", "yellow", "green", "black", "pink"]; // 0 4
+
+function random(e) {
+  console.log(e.target);
+  const randomIndex = Math.floor(Math.random() * array.length);
+  const container = document.getElementById("container");
+  container.style.backgroundColor = array[randomIndex];
+}
+
+const button = document.createElement("button");
+
+button.textContent = "change color";
+button.style.marginTop = "10px";
+document.body.appendChild(button);
+
+button.addEventListener("click", random);
