03bff7e828aa8b3af0c4ea8dc8f9e8514a5dee0c
Vishal Sharma <vishalsharma@ip-192-168-0-105.ap-south-1.compute.internal>
2023-07-13 21:34:39 +0530

    event capture

--- eventbubbling.js
+++ eventbubbling.js
@@ -37,4 +37,23 @@ function checkBoxHandler(event) {
   alert("Checkobox clicked");
 }
 
-// immediateStopPropagation()
+//event capturing
+
+// In this case event will triggered and execute from parent to child.
+// By default behaviour of browser is bubble the event(event bubbling , bottom to down) , by pasing third parameter true on addEventListener you can achive event captuing
+
+document.getElementById("capture").addEventListener(
+  "click",
+  () => {
+    alert("hey I am capture parent");
+  },
+  true
+);
+
+document.getElementById("childCap").addEventListener(
+  "click",
+  () => {
+    alert("hey I am capture child");
+  },
+  true
+);
