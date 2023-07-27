846247cb2c1dc67c7ba58531a5dae7830bab5b61
Vishal Sharma <vishal.sharma@zeptonow.com>
2023-07-27 22:47:33 +0530

    call and aplly difference

--- this.js
+++ this.js
@@ -195,3 +195,34 @@ const doctor1 = {
 doctor.printFullName();
 
 doctor.printFullName.call(doctor1); //
+doctor.printFullName.apply(doctor1); //
+
+// prototype  => bind  , call , apply/
+
+// In call and apply first argument will be this for the function which is using call and apply
+// there is differec in second argument  in apply you have to pass the argument as array of list
+// but in the case of call you can pass the seperated argument
+
+function getEmployeeSalary(variable, tds) {
+  console.log(this, variable, tds);
+}
+
+const emp1 = {
+  firstName: "Vishal",
+  increment: "30%",
+  salary: 10000,
+};
+const emp2 = {
+  firstName: "Timo",
+  increment: "10%",
+  salary: 100000,
+};
+const emp3 = {
+  firstName: "Rimo",
+  increment: "20%",
+  salary: 200000,
+};
+getEmployeeSalary.call(emp1, 400000, 30);
+getEmployeeSalary.apply(emp1, [400000, 30, 134, 3445]);
+getEmployeeSalary.call(emp1, 20000, 30);
+getEmployeeSalary.apply(emp1, [300000, 30]);
