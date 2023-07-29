4c5419fd028d00661960d78a67301c2dbe9ca918
Vishal Sharma <vishal.sharma@zeptonow.com>
2023-07-29 22:13:06 +0530

    propertyIsEnumerable

--- propertydescriptor.js
+++ propertydescriptor.js
@@ -9,7 +9,7 @@ console.log(dsecriptor, "dsecriptor");
 
 // configurable  // true if this is true than value of key can be change else it will be read only
 // value// the value of key
-// enumerable // true  // while traversing the object you want to give value or not
+// enumerable // true  // while traversing the object if this is false you will not get that propertyName
 // writable // true // modification  deletion
 
 // how to update these value
@@ -25,7 +25,7 @@ for (let name in employee) {
 Object.defineProperty(employee, "firstName", {
   writable: false,
   enumerable: false,
-  configurable: false,
+  //   configurable: false,
 });
 
 // this will throw error you cannot update the propertyName again
@@ -33,12 +33,19 @@ Object.defineProperty(employee, "firstName", {
 // configurable : false means cannot modify cannot delete  cannot definedProperty again
 Object.defineProperty(employee, "firstName", {
   configurable: true,
+  writable: true,
 });
 for (let name in employee) {
   console.log(name, "here after making enumerable:false to firstName");
 }
-
 let dsecriptor1 = Object.getOwnPropertyDescriptors(employee);
-
-employee.firstName = "Rimo";
 console.log(dsecriptor1);
+employee.firstName = "Rimo";
+let dsecriptor2 = Object.getOwnPropertyDescriptors(employee);
+console.log(dsecriptor2);
+console.log(employee);
+
+// whether the object property is enumerable or not
+
+console.log(employee.propertyIsEnumerable("firstName"));
+console.log(employee.propertyIsEnumerable("lastName"));
