31f03f40fa74697e7bd9daeff35bb0a72115a8bb
Vishal Sharma <vishal.sharma@zeptonow.com>
2023-08-01 22:20:55 +0530

    __proto__

--- prototype.js
+++ prototype.js
@@ -1,6 +1,16 @@
 //
-const employee = {
-  name: "Vishal",
+
+let animal = {
+  eats: true,
 };
 
-console.log(employee);
+let cow = {
+  sound: "bow bow !!",
+};
+
+cow.__proto__ = animal; // here i am assigning [[prototype]] of cow is equal
+// animal.__proto__ = cow; // cyclic proto value error
+//  to the animal  now cow can access the propery of animal;
+
+console.log(cow);
+console.log(animal);
