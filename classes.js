c0903c7c7dbf73e29b49ad0fba645176b3ee7d1f
Vishal Sharma <vishal.sharma@zeptonow.com>
2023-08-05 22:22:35 +0530

    super constructor

--- classes.js
+++ classes.js
@@ -48,35 +48,43 @@ console.log(emp1.getName);
 // privarte
 
 class Vehicle {
-  //   constructor(brand, name, color, tyre) {
-  //     this.brand = brand;
-  //     this.name = name;
-  //     this.color = color;
-  //     this.tyre = tyre;
-  //   }
-  //   getVehicleName() {
-  //     return this.name;
-  //   }
-  //   getVehicleColor() {
-  //     return this.color;
-  //   }
-  //   getVehicleTyre() {
-  //     return this.tyre;
-  //   }
+  constructor(brand, name, color, tyre) {
+    this.brand = brand;
+    this.name = name;
+    this.color = color;
+    this.tyre = tyre;
+  }
+  getVehicleName() {
+    return this.name;
+  }
+  getVehicleColor() {
+    return this.color;
+  }
+  getVehicleTyre() {
+    return this.tyre;
+  }
   speed(value) {
     console.log("this is the speed", value);
   }
 }
 
 class FourWheeler extends Vehicle {
+  constructor(...args) {
+    // args  =>
+
+    super(...args);
+  }
   getMileage(value) {
     console.log("this is the mileage", value);
   }
+  // overriding
+  getVehicleTyre() {
+    return super.getVehicleTyre();
+  }
 }
 
-const four = new FourWheeler();
+const four = new FourWheeler("mahindra", "xuv700", "black", 4);
 four.speed(200);
 four.getMileage(400);
 
-const vehicle = new Vehicle("mahindra", "xuv700", "black", 4);
-console.log(vehicle);
+console.log(four);
