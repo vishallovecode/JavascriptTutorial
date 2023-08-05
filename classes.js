6c6fdeb75f93c5325b9b76663cb7b22617a74164
Vishal Sharma <vishal.sharma@zeptonow.com>
2023-08-05 22:09:21 +0530

    inheritance

--- classes.js
+++ classes.js
@@ -38,4 +38,45 @@ emp1.printName();
 
 console.log(emp1.getName);
 
-//
+//  20%
+// inheritance
+// polymorphism
+// overriding
+// super
+// constructor
+// parent method
+// privarte
+
+class Vehicle {
+  //   constructor(brand, name, color, tyre) {
+  //     this.brand = brand;
+  //     this.name = name;
+  //     this.color = color;
+  //     this.tyre = tyre;
+  //   }
+  //   getVehicleName() {
+  //     return this.name;
+  //   }
+  //   getVehicleColor() {
+  //     return this.color;
+  //   }
+  //   getVehicleTyre() {
+  //     return this.tyre;
+  //   }
+  speed(value) {
+    console.log("this is the speed", value);
+  }
+}
+
+class FourWheeler extends Vehicle {
+  getMileage(value) {
+    console.log("this is the mileage", value);
+  }
+}
+
+const four = new FourWheeler();
+four.speed(200);
+four.getMileage(400);
+
+const vehicle = new Vehicle("mahindra", "xuv700", "black", 4);
+console.log(vehicle);
