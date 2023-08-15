e26371f3a9c9d2cbc556ab49a13fc63a30f61900
Vishal Sharma <vishal.sharma@zeptonow.com>
2023-08-15 22:48:02 +0530

    wip promise

--- Promises.js
+++ Promises.js
@@ -25,17 +25,25 @@ console.log("hry");
 // const promise1 = new Promise(callBack);
 
 const promise1 = new Promise((res, rej) => {
+  console.log("Hey");
   setTimeout(() => {
+    console.log("Hey123");
     rej("Hey I am rejected");
   }, 4000);
 });
 
 console.log(promise1);
+// promise will never give you the result either it will give
 
-promise1
-  .then(() => {
-    console.log("hey");
-  })
-  .catch(() => {
-    console.log("rejected");
-  });
+//
+// promise1
+//   .then(() => {
+//     console.log("hey");
+//   })
+//   .catch(() => {
+//     console.log("rejected");
+//   });
+
+// setTimeout(() => {
+//   console.log("5 second", promise1);
+// }, 5000);
