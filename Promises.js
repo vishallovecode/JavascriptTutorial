f956e10f5d8dd7b311fe67bdd8fddf6d86217acb
Vishal Sharma <vishal.sharma@zeptonow.com>
2023-08-17 22:31:26 +0530

    notes

--- Promises.js
+++ Promises.js
@@ -142,8 +142,13 @@ promise1235.then(
 );
 
 const getAllProducts = () => {
+  // fetch is browser api which return promises everytime
+  // by default Request method of fetch is GET
   const response = fetch("https://dummyjson.com/products");
+  // response will be here promise i need promise chaining chaining for resolving
   response.then((res) => {
+    // here res value is not in json format to convert it into json format we need res.json() function , this json() return promises we need to resolved it agai using
+    //  promise chaining
     return res.json().then((response) => {
       console.log("response", response);
     });
