82557b8e90fe57b51ee59482ffe0be392c03a27f
Vishal Sharma <vishal.sharma@zeptonow.com>
2023-08-30 22:59:37 +0530

    debounce

--- debounce.js
+++ debounce.js
@@ -8,3 +8,20 @@ async function getUserData(searchValue) {
 function handleInputChange(event) {
   getUserData(event.target.value);
 }
+
+// debounce function take two parameter first which we need to debounced  and another delay
+// it returns a new function
+function debounce(func, delay) {
+  let timer;
+  return function (...args) {
+    if (!timer) {
+      func.call(this, ...args);
+      //   func.apply(this, args);
+      //   func(...args)
+    }
+    timer = setTimeout(() => {}, delay);
+  };
+}
+
+const debounceSearch = debounce(handleInputChange, 1000);
+debounceSearch("hello");
