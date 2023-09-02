b45546797b6823991c677c1dc69e7aac02854779
Vishal Sharma <vishal.sharma@zeptonow.com>
2023-09-02 22:30:00 +0530

    local storage

--- BrowserStorage.js
+++ BrowserStorage.js
@@ -11,12 +11,51 @@ if (typeof window != "undefined") {
 }
 
 // removing  a value from local storage
-if (typeof window != "undefined") {
-  localStorage.removeItem("key1");
-  // here in setItem first params is key and second is value , value can be any type
-}
+// if (typeof window != "undefined") {
+//   localStorage.removeItem("key1");
+//   // here in setItem first params is key and second is value , value can be any type
+// }
 
-if (typeof window != "undefined") {
-  console.log(localStorage.getItem("key1"));
-  // here in setItem first params is key and second is value , value can be any type
-}
+// if (typeof window != "undefined") {
+//   console.log(localStorage.getItem("key1"));
+//   // here in setItem first params is key and second is value , value can be any type
+// }
+
+const employeeDetails = {
+  user: {
+    name: "Vishal",
+    address: {
+      india: {
+        state: {
+          city: {
+            district: {
+              name: "Kanpur",
+            },
+          },
+        },
+      },
+    },
+  },
+};
+
+// localStorage.setItem("userDetails", employeeDetails);
+// const fetchData = localStorage.getItem("userDetails");
+// console.log(fetchData.user);
+
+localStorage.setItem("userDetails", JSON.stringify(employeeDetails));
+
+console.log(typeof localStorage.getItem("userDetails"));
+
+const fetchData = JSON.parse(localStorage.getItem("userDetails"));
+
+console.log(fetchData.user);
+
+const value1 = { name: "Vishal" };
+console.log(value1.name); // Vishal
+const value = JSON.stringify({ name: "Vishal" });
+console.log(value.name); // Vishal
+console.log(typeof value);
+
+const jsonValue = JSON.parse(value);
+// JSON.parse() arguments should be object and that should be string
+console.log(jsonValue.name);
